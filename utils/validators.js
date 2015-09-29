6751d5d3645fe4f454e238abedc6f53ac5335cd5
David Josefson <david.josefson@gmail.com>
2015-09-29 23:09:47 +0200

    Fixed error message from validators

--- utils/validators.js
+++ utils/validators.js
@@ -3,27 +3,27 @@ var constants = require('../constants/constants');
 var validators = {};
 
 validators.room = function(room)  {
-  var room;
+  var validRoom;
   if (room === null || room === undefined) {
     return new Error('No room was specified.');
   } else if (typeof room != 'string')  {
     return new Error('Room was not a string.'); 
-  } else if ((room = constants.ROOMS[room]) === undefined) {
-    return new Error('"' + room + '" is not a valid room. See endpoint /rooms for a list of valid rooms to use');
+  } else if ((validRoom = constants.ROOMS[room]) === undefined) {
+    return new Error('\'' + room + '\' is not a valid room. See endpoint /rooms for a list of valid rooms to use.');
   }
-  return room;
+  return validRoom;
 }
 
 validators.time = function(time) {
-  var time;
+  var validTime;
   if (time === null ||  time === undefined) {
     return new Error('No time was specified.');
   } else if (typeof time != 'string')  {
     return new Error('Time was not a string.'); 
-  } else if ((time = constants.TIMES[time]) === undefined) {
-    return new Error('"' + time + '" is not a valid time. See endpoint /times for a list of valid times to use');
+  } else if ((validTime = constants.TIMES[time]) === undefined) {
+    return new Error('\'' + time + '\' is not a valid room. See endpoint /times for a list of valid times to use.');
   }
-  return time;
+  return validTime;
 }
 
 validators.date = function(date) {
@@ -32,7 +32,7 @@ validators.date = function(date) {
   } else if (typeof date != 'string')  {
     return new Error('Date was not a string.');
   } else if (!date.match(/\b(\d{4})-(\d{2})-(\d{2})\b/))  {
-    return new Error('"' + date + '" is not a valid date. Has to be in ISO 8106-format YYYY-MM-DD. For example "2016-01-15"');
+    return new Error('\'' + date + '\' is not a valid date. Has to be in ISO 8106-format YYYY-MM-DD, example: \'2016-01-15\'');
   }
   return date;
 }

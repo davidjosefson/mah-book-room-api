a06e9e0f75c9a4c30fbe7faf39619c918f61ea8f
David Josefson <david.josefson@gmail.com>
2015-09-29 23:16:00 +0200

    Errors are now json instead of using jade template

--- app.js
+++ app.js
@@ -43,24 +43,34 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
+// if (app.get('env') === 'development') {
+//   app.use(function(err, req, res, next) {
+//     res.status(err.status || 500).json({
+//       message: err.message,
+//       error: err
+//     });
+//
+//     // res.render('error', {
+//     //   message: err.message,
+//     //   error: err
+//     // });
+//   });
+// }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
+  var status = (err.status ||  500);
+  res.status(status).json({
+    status: status,
+    error: err.message,
   });
+
+  // res.status(err.status || 500);
+  // res.render('error', {
+  //   message: err.message,
+  //   error: {}
+  // });
 });
 
 

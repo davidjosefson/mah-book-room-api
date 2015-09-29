b279531f93bd56dbc2c737bc15587a56409ce4e0
David Josefson <david.josefson@gmail.com>
2015-09-29 22:59:52 +0200

    Added better error messages/codes when failing to book a room

--- routes/bookings.js
+++ routes/bookings.js
@@ -12,22 +12,107 @@ router.get('/', validateUserAndPass, function(req, res, next) {
 
 /* POST to /bookings: create a booking */
 router.post('/', validateUserAndPass, validatePostBody, function(req, res, next) {
-  /*
-    2. perform request-get to schema.mah.se
-    3. create a json-response of the booking information
-  */
-
   performExternalBooking(req.userAndPassValidationResult, req.postBodyValidationResult, function(err, result)  {
     if (!err) {
-      var booking = bookingModel.getSingleBooking(req.body.room, req.body.date, req.body.time);
+      var booking = bookingModel.getSingleBooking(req.postBodyValidationResult.room.id, req.postBodyValidationResult.date, req.postBodyValidationResult.time.id);
       res.json(booking);
     } else  {
-      res.status(500).json({
-        error: err
+      res.status(err.code).json({
+        error: err.message
       });
     }
   });
 });
+
+function performExternalBooking(userAndPassword, postBodyObjects, callback)  {
+  var user = userAndPassword.name;
+  var pass = userAndPassword.pass;
+  var room = postBodyObjects.room.urlRoom;
+  var time = postBodyObjects.time.urlTime;
+  var date = stripTwoDigitsFromYear(postBodyObjects.date);
+
+  // sets a new cookie jar for each request
+  var j = request.jar();
+
+  // Handshake request: to get a JSESSIONID-cookie
+  request({
+    url: 'https://schema.mah.se/resursbokning.jsp?flik=FLIK-0017',
+    jar: j
+  }, function(err, httpResponse, bodyHandshake) {
+    // Login request (using the cookie from the handshake)
+    if (!err) {
+      request({
+        method: 'POST',
+        url: 'https://schema.mah.se/login_do.jsp',
+        form: {
+          username: user,
+          password: pass
+        },
+        jar: j
+      }, function(err, httpResponse, body) {
+        // Booking request
+        if (!err) {
+          var url = 'https://schema.mah.se/ajax/ajax_resursbokning.jsp?op=boka&datum=' + date + '&id=' + room + '&typ=RESURSER_LOKALER&intervall=' + time + '&moment= &flik=FLIK-0017';
+          request({
+            url: url,
+            jar: j
+          }, function(err, httpResponse, body) {
+            if (!err) {
+              if (body != 'OK') {
+
+                if (body.match(/\bBokningen gick inte att spara pga kollision med följande resurser\b/i))  {
+                  var error = new Error('The chosen room was already booked for that date and time. Room ' + postBodyObjects.room.name + ', date ' + postBodyObjects.date + ', time ' + postBodyObjects.time.name);
+                  error.code = 409; // conflict
+                  callback(error, null);
+
+                } else if (body.match(/\bDin användare har inte rättigheter att skapa resursbokningar\b/i)) {
+                  var error = new Error('Your user credentials aren\'t valid');
+                  error.code = 401; // authentication failed
+                  callback(error, null);
+
+                } else if (body.match(/\bEj bokbar\b/i)) {
+                  var error = new Error('Internal error. schema.mah.se didn\'t accept the url parameters.');
+                  error.code = 500;
+                  callback(error, null);
+
+                } else {
+                  // 200 OK but not a known error message from schema.mah.se
+                  var error = new Error('The following error was received from schema.mah.se: ' + body);
+                  error.code = 500;
+                  callback(error, null);
+                }
+
+              } else {
+                // Everything OK, booking was successful
+                callback(null, true);
+              }
+            } else {
+              // If booking request failed (schema.mah.se sent something other than 200 OK)
+              var error = new Error('Internal error. Booking request failed, schema.mah.se/ajax/ajax_resursbokning.jsp is probably out of reach. This message was sent from schema.mah.se: ' + err.message);
+              error.code = 500;
+              callback(error, null);
+            }
+          });
+        } else {
+          // If login request failed
+          var error = new Error('Login request to schema.mah.se failed. Either your login credentials are wrong or schema.mah.se/login_do.jsp is out of reach.');
+          error.code = 500;
+          callback(error, null);
+        }
+      });
+
+    } else {
+      // If handshake request failed
+      var error = new Error('Internal error. Handshake request failed, schema.mah.se is probably down.');
+      error.code = 500;
+      callback(error, null);
+    }
+  });
+}
+
+var stripTwoDigitsFromYear = function(date) {
+  return date.substring(2);
+}
 
 function validateUserAndPass(req, res, next) {
   var user = basicAuth(req);
@@ -75,68 +160,6 @@ function validatePostBody(req, res, next) {
 
     next();
   }
-}
-
-function performExternalBooking(userAndPassword, postBodyObjects, callback)  {
-  var user = userAndPassword.name;
-  var pass = userAndPassword.pass;
-  var room = postBodyObjects.room.urlRoom;
-  var time = postBodyObjects.time.urlTime;
-  var date = stripTwoDigitsFromYear(postBodyObjects.date);
-
-  // sets a new cookie jar for each request
-  var j = request.jar();
-
-  request({
-    url: 'https://schema.mah.se/resursbokning.jsp?flik=FLIK-0017',
-    jar: j
-  }, function(err, httpResponse1, body) {
-    request({
-      method: 'POST',
-      url: 'https://schema.mah.se/login_do.jsp',
-      form: {
-        username: user,
-        password: pass
-      },
-      jar: j
-    }, function(err, httpResponse2, body) {
-      if (!err) {
-        var url = 'https://schema.mah.se/ajax/ajax_resursbokning.jsp?op=boka&datum=' + date + '&id=' + room + '&typ=RESURSER_LOKALER&intervall=' + time + '&moment= &flik=FLIK-0017';
-        request({
-          url: url,
-          jar: j
-        }, function(err, httpResponse3, body) {
-          if (!err) {
-            if (body != 'OK') {
-              if (body.match(/\bBokningen gick inte att spara pga kollision med följande resurser\b/i))  {
-                callback('The chosen room was already booked for that date and time. Room: ' + postBodyObjects.room.name + ' Date: ' + postBodyObjects.date + ' Time: ' + postBodyObjects.time.name, null);
-              } else if (body.match(/\bDin användare har inte rättigheter att skapa resursbokningar\b/i)) {
-                callback('Your user credentials aren\'t valid', null);
-              } else if (body.match(/\bEj bokbar\b/i)) {
-                callback('Internal error: schema.mah.se didn\t accept the url parameters.')
-              } else {
-                callback('The following error was received from schema.mah.se: ' + body, null);
-              }
-            } else {
-              // console.log('Room ' + args.room + ' was booked at ' + args.date + ' ' + constants.TIMES[args.time].readableTime);
-              callback(null, true);
-            }
-          } else {
-            console.log('GET for https://schema.mah.se/ajax/ajax_resursbokning.jsp?.. failed: ', err);
-            callback(err, null);
-          }
-        });
-      } else {
-        console.log('POST for https://schema.mah.se/login_do.jsp.. failed: ', err);
-
-        callback(err, null);
-      }
-    });
-  });
-}
-
-var stripTwoDigitsFromYear = function(date) {
-  return date.substring(2);
 }
 
 module.exports = router;

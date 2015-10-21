87a82ff7b739981c75213ee235747d4c08fff43a
David Josefson <david.josefson@gmail.com>
2015-10-21 16:12:30 +0200

    Removed comments

--- app.js
+++ app.js
@@ -44,24 +44,6 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500).json({
-//       message: err.message,
-//       error: err
-//     });
-//
-//     // res.render('error', {
-//     //   message: err.message,
-//     //   error: err
-//     // });
-//   });
-// }
-
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
@@ -70,13 +52,6 @@ app.use(function(err, req, res, next) {
     status: status,
     error: err.message,
   });
-
-  // res.status(err.status || 500);
-  // res.render('error', {
-  //   message: err.message,
-  //   error: {}
-  // });
 });
-
 
 module.exports = app;

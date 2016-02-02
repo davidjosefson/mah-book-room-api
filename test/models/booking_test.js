79168e5572417babd678a80d716f44d17aa205b0
David Josefson <david.josefson@gmail.com>
2016-02-02 17:02:26 +0100

    Fixed failing comment test

--- test/models/booking_test.js
+++ test/models/booking_test.js
@@ -5,15 +5,16 @@ describe('Testing the booking model', function()  { 
   describe('Testing the getSingleBooking()-function', function() {
     var booking;
     before(function() {
-      booking = bookingModel.getSingleBooking('nia0506', '2015-06-12', '17152000');
+      booking = bookingModel.getSingleBooking('nia0506', '2015-06-12', '17152000', 'testcomment');
     });
 
-    it('should only have the following keys: id, date, _embedded and _links', function()  {
-      expect(booking).to.have.all.keys(['id', 'date', '_embedded', '_links']);
+    it('should only have the following keys: id, date, comment, _embedded and _links', function()  {
+      expect(booking).to.have.all.keys(['id', 'date', 'comment', '_embedded', '_links']);
     });
     it('should return an object with properties id, date, _embedded and _links', function() {
       expect(booking).to.have.property('id').that.equals('nia05062015-06-1217152000');
       expect(booking).to.have.property('date').that.equals('2015-06-12');
+      expect(booking).to.have.property('comment').that.equals('testcomment');
     });
     it('should return an object where the _embedded room-object to have correct properties', function() {
       expect(booking).to.have.deep.property('_embedded.room.id', 'nia0506');
